933b4cd7e48c18f2be6ee1cef38b82416c81f828
Danis Abadia <54759585+mrthoabby@users.noreply.github.com>
2023-12-04 21:50:17 -0500

    Update install_freeswitch.sh
    - Add symbolic links

--- install_freeswitch.sh
+++ install_freeswitch.sh
@@ -23,5 +23,28 @@ apt-get update && apt-get install -y freeswitch-meta-all
 # Get installed Freeswitch version
 INSTALLED_VERSION=$(dpkg -l | grep freeswitch-meta-all | awk '{print $3}')
 
+# Check if links should be created
+if [ "$CREATE_LINKS" = true ]; then
+    # Create symbolic links in /usr/local/freeswitch/bin
+    mkdir -p /usr/local/freeswitch/bin
+    ln -s /usr/bin/freeswitch /usr/local/freeswitch/bin/
+    ln -s /usr/bin/fs_* /usr/local/freeswitch/bin/
+    ln -s /usr/bin/fsxs /usr/local/freeswitch/bin/
+    ln -s /usr/bin/tone2wav /usr/local/freeswitch/bin/
+    ln -s /usr/bin/gentls_cert /usr/local/freeswitch/bin/
+    ln -s /etc/freeswitch /usr/local/freeswitch/conf
+    ln -s /var/lib/freeswitch/* /usr/local/freeswitch/
+    ln -s /var/log/freeswitch /usr/local/freeswitch/log
+    ln -s /var/run/freeswitch /usr/local/freeswitch/run
+    ln -s /etc/freeswitch/tls /usr/local/freeswitch/certs
+    ln -s /usr/lib/freeswitch/mod /usr/local/freeswitch/
+    rm /usr/local/freeswitch/lang
+
+    echo "Se crearon los enlaces simbólicos en /usr/local/freeswitch/"
+else
+    echo "No se crearon enlaces simbólicos. Para crearlos, proporciona el parámetro 'join' después del token."
+fi
+
+
 echo "Freeswitch was installed successfully."
 echo "Installed version: $INSTALLED_VERSION"
